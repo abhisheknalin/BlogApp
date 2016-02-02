3288ff8bc8e9a7abb63902b06e531cd6007d3bf9
abhishek nalin <abhishek.nalin@gmail.com>
2016-02-02 11:13:51 +0530

    updated imports

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
     "net/http"
     "time"
 
-    "git-go-websiteskeleton/app/common"
-    "git-go-websiteskeleton/app/home"
-    "git-go-websiteskeleton/app/user"
+    "blogapp/common"
+    "blogapp/home"
+    "blogapp/user"
 
     "github.com/golang/glog"
     "github.com/gorilla/mux"
