2007aaddc48384ffe3fe5914d29818f332fb5010
abhishek nalin <abhishek.nalin@gmail.com>
2016-02-03 19:20:44 +0530

    added new pages

---
+++ home/aboutus.go
@@ -0,0 +1,22 @@
+package home
+
+import (
+    "net/http"
+    "html/template"
+
+    "github.com/abhisheknalin/blogapp/common"
+)
+
+func GetAboutusPage(rw http.ResponseWriter, req *http.Request) {
+    type Page struct {
+        Title string
+    }
+    
+    p := Page{
+        Title: "Aboutus",
+    }
+
+    common.Templates = template.Must(template.ParseFiles("templates/home/home.html", common.LayoutPath))
+    err := common.Templates.ExecuteTemplate(rw, "base", p)
+    common.CheckError(err, 2)
+}
